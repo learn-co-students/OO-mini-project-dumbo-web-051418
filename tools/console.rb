c80d7c31abb62eddee473b605b80fcd74a4bbcca
prmoreira23 <prm.gredes@gmail.com>
2018-05-24 15:07:41 -0400

    First version

--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,19 @@
 require_relative '../config/environment.rb'
 
+user = User.new("Pablo", 21)
+user2 = User.new("Jathmel", 25)
+
+recipe = Recipe.new("Chicken")
+recipe2 = Recipe.new("Steamed Pork")
+
+rc = RecipeCard.new(user, recipe2, 4.5)
+
+in2 = Ingredient.new("flour")
+in1 = Ingredient.new("peas")
+in3 = Ingredient.new("carrots")
+
+recipe.add_ingredients([in1, in2, in3])
+
+a = Allergen.new(user2, in2)
+
 binding.pry
